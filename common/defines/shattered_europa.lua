5716402a01ddda83532179f8b47fcc1fa77831cc
Nep <38312408+Neprut@users.noreply.github.com>
2018-04-15 18:00:30 -1000

    Nep 002
    England + Franh ideas back
    Localizaition will be back later

---
+++ common/defines/shattered_europa.lua
@@ -0,0 +1,7 @@
+NDefines.NGame.START_DATE = "1399.01.01" -- EU3 Start Date - plan on making EU3 a start when I have time
+NDefines.NGame.END_DATE = "3018.04.15" -- 100 years from today
+NDefines.NNationDesigner.POINTS_AVAILABLE = 500000
+NDefines.NNationDesigner.RULER_MIN_AGE = 1
+NDefines.NNationDesigner.RULER_MAX_AGE = 60
+NDefines.NNationDesigner.HEIR_MAX_AGE = 40
+NDefines.NCountry.ALLOW_FEMALE_GENERALS = 1
